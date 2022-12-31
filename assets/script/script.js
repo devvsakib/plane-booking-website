b106e7ded6e6eb081b3b96ee4ecfdf6a439b83b0
DevvSakib <devvsakib@gmail.com>
2023-01-01 01:34:39 +0600

    hero section done

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+(window.addEventListener("scroll", function () {
+    // Get the current scroll position
+    const scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Update the size of the plane
+    const plane = document.getElementById("plane");
+    plane.style.width = (500 + scrollPos) + "px";
+    plane.style.bottom = ((scrollPos + 150) + 50) + "px";
+    plane.style.left = ((scrollPos + 450) + 50) + "px";
+    console.log(plane.style.top,plane.style.left);
+}))()
